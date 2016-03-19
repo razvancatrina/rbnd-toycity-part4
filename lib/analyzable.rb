09c69e5771a82be963fc9e2dd6795c2a24593cf7
Razvan Catrina <razvan.catrina@gmail.com>
2016-03-19 07:57:37 +0200

    add print report

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -11,4 +11,41 @@ module Analyzable
   	rounded_sum = sum.round(2)
   	average_price = (rounded_sum/product_list.length).round(2)
   end
+
+  def print_report(product_list)
+  	report = ""
+
+  	report += get_average_price_report(product_list)
+  	report += get_inventory_by_brand_report(product_list)
+  	report += get_inventory_by_name_report(product_list)
+  	
+  	return report
+  end
+
+  def get_average_price_report(product_list)
+  	average_price = average_price(product_list)
+  	"Average Price: $" + average_price.to_s + "\n"
+  end
+
+  def get_inventory_by_brand_report(product_list)
+  	report_details = "Inventory by Brand:\n"
+  	brands = product_list.group_by {|i| i.brand}
+  	
+  	brands.each do |key, value|
+  	report_details += "- #{key}: #{value.length}\n"
+  	end
+
+  	return report_details
+  end
+
+  def get_inventory_by_name_report(product_list)
+  	report_details = "Inventory by Name:\n"
+  	brands = product_list.group_by {|i| i.name}
+  	
+  	brands.each do |key, value|
+  	report_details += "- #{key}: #{value.length}\n"
+  	end
+
+  	return report_details
+  end
 end
