d21614f53dcd71b2f1091118c300eacd9b6eb86f
razvancatrina <razvan.catrina@gmail.com>
2016-03-24 21:41:03 +0200

    Update find_by.rb

--- lib/find_by.rb
+++ lib/find_by.rb
@@ -3,7 +3,7 @@ class Module
      attributes.each do |attribute|
       self.instance_eval(
       	"def find_by_#{attribute}(param)
-      		product = Product.all.select{ |item| item.#{attribute} == param}.first
+      		product = self.all.select{ |item| item.#{attribute} == param}.first
       		return product
       	end"
       )
